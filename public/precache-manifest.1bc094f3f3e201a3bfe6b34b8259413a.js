410b6732c4801eac89d8771319bccdec7432aabb
Lor3nzoMartinez <Lorenzo.s.martinez0@gmail.com>
2019-08-28 15:18:07 -0700

    Heroku init

---
+++ public/precache-manifest.1bc094f3f3e201a3bfe6b34b8259413a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4d0099995e55dbe5f879b9ebab5295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7494db7dcc9df3845592",
+    "url": "/static/css/main.6c58235d.chunk.css"
+  },
+  {
+    "revision": "5344cb4aab8fa5a6ac7f",
+    "url": "/static/js/2.ae44c92b.chunk.js"
+  },
+  {
+    "revision": "7494db7dcc9df3845592",
+    "url": "/static/js/main.44d93211.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
